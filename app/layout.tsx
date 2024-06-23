b8a76a28ea83e375bc74c48ce893ccfe328dcd2b
Spet77 <76452706+Yelloo5191@users.noreply.github.com>
2024-06-23 00:14:01 -0700

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,20 +10,26 @@ import { fonts } from "./fonts";
 import Layout from "@/components/Layout";
 
 export const metadata: Metadata = {
-  title: "Melo",
-  description: "A music discovery platform.",
+    title: "Melo",
+    description: "A music discovery platform.",
+    icons: [
+        {
+            url: "/favicon.ico",
+            sizes: "48x48 72x72 96x96 128x128 256x256",
+        },
+    ],
 };
 
 export default async function RootLayout({ children }: any) {
-  const session = await auth();
+    const session = await auth();
 
-  return (
-    <html lang="en" className={fonts.poly.className}>
-      <body>
-        <Providers session={session}>
-          <Layout>{children}</Layout>
-        </Providers>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" className={fonts.poly.className}>
+            <body>
+                <Providers session={session}>
+                    <Layout>{children}</Layout>
+                </Providers>
+            </body>
+        </html>
+    );
 }
