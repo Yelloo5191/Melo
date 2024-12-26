180512f4ef73272bd9b294178db6f385470e0fc5
Hovhannes Muradyan <76452706+Yelloo5191@users.noreply.github.com>
2024-12-26 13:08:26 -0800

    fixed annoying lack of margin on hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,45 +7,46 @@ import Logo from "./Logo";
 import { signIn, useSession } from "next-auth/react";
 
 function Hero() {
-    const { data: session } = useSession();
-    return (
-        <VStack
-            className="hero my-5 text-center"
-            data-testid="hero"
-            width={{ base: "90%", md: "50%" }}
-            mx="auto"
-        >
-            <Logo testId="hero-logo" />
-            <Heading data-testid="hero-title" textAlign="center">
-                Find your <mark>noise.</mark>
-            </Heading>
-            <Text
-                mb="2rem !important"
-                data-testid="hero-lead"
-                fontSize="xl"
-                textAlign="center"
-                w="50%"
-            >
-                Melo is a <mark>music discovery</mark> platform that helps you
-                find new songs based on your listening habits.
-            </Text>
-            <Button
-                bg="brand.logo.light"
-                color="brand.colors.primary.text"
-                _hover={{ bg: "brand.logo.background" }}
-                boxShadow="0px 0px 100px var(--chakra-colors-brand-logo-light)"
-                variant="outline"
-                size="lg"
-                data-testid="hero-button"
-                onClick={() => {
-                    if (session) window.location.href = "/dashboard";
-                    else signIn("spotify");
-                }}
-            >
-                Get started
-            </Button>
-        </VStack>
-    );
+  const { data: session } = useSession();
+  return (
+    <VStack
+      className="hero"
+      data-testid="hero"
+      width={{ base: "90%", md: "50%" }}
+      mx="auto"
+      mb={{ base: "2rem", md: "5rem" }}
+    >
+      <Logo testId="hero-logo" />
+      <Heading data-testid="hero-title" textAlign="center">
+        Find your <mark>noise.</mark>
+      </Heading>
+      <Text
+        mb="2rem !important"
+        data-testid="hero-lead"
+        fontSize="xl"
+        textAlign="center"
+        w="50%"
+      >
+        Melo is a <mark>music discovery</mark> platform that helps you find new
+        songs based on your listening habits.
+      </Text>
+      <Button
+        bg="brand.logo.light"
+        color="brand.colors.primary.text"
+        _hover={{ bg: "brand.logo.background" }}
+        boxShadow="0px 0px 100px var(--chakra-colors-brand-logo-light)"
+        variant="outline"
+        size="lg"
+        data-testid="hero-button"
+        onClick={() => {
+          if (session) window.location.href = "/dashboard";
+          else signIn("spotify");
+        }}
+      >
+        Get started
+      </Button>
+    </VStack>
+  );
 }
 
 export default Hero;
